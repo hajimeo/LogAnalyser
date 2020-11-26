d7fb0e88cb2810bc95b0e20055ba6d0cc99814f7
hajime <github@hajigle.com>
2020-11-26 12:51:08 +1000

    commiting for testing...

--- script/setup.sh
+++ script/setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+type _import &>/dev/null || _import() { [ -s /tmp/${1} ] || curl -sf --compressed "https://raw.githubusercontent.com/hajimeo/samples/master/bash/$1" -o /tmp/${1}; . /tmp/${1}; }   # not using _$$
+
+_import "utils.sh"
+_import "_setup_host.sh"    # functions for Ubuntu OS
+_import "setup_work_env.sh" # functions for applicaitons (python, golang, java ...)
+
+
 function usage() {
-    echo "Setup and start web server
+    echo "Setup and start service for Log analysis
 USAGE:
     $BASH_SOURCE
 or
@@ -13,100 +20,15 @@ NOTE:
 }
 
 
-### Global variables ###########################################################
-_ROOT_DIR="$(dirname "$(dirname "$BASH_SOURCE")")"
-_HOST="0.0.0.0"
-_PORT="48080"
+### Global variables for this script ##########################################
+: ${_APP_USER:="loganalyser"}
+: ${_INST_DIR:="/var/www/${_APP_USER%/}"}
 
 
 ### Executable functions with -f ###############################################
-function f_validations() {
-    _is_in_path "rsync" || return $?
-    _is_in_path "curl" || return $?
-    _is_in_path "rg" || return $?
-    _is_in_path "pip" || return $?
-    _is_in_path "php-fpm" || return $?
-
-    _exist "/etc/php-fpm.conf" || return $?
-    if ! _exist "/etc/php-fpm.d/www.conf" "" ; then
-        if [ ! -s "/etc/php-fpm.d/www.conf.default" ] || ! cp -p /etc/php-fpm.d/www.conf.default /etc/php-fpm.d/www.conf; then
-            _log "ERROR" "No /etc/php-fpm.d/www.conf"
-            return 1
-        fi
-    fi
-}
-
-function f_web_start() {
-    local _root_dir="${1:-${_ROOT_DIR}}"
-    if [ ! -d "${_root_dir%/}/log" ]; then
-        if ! mkdir -p -m 777 ${_root_dir%/}/log; then
-            _log "ERROR" "Couldn't create ${_root_dir%/}/log"
-            return 1
-        fi
-    fi
-
-    if [ ! -s "${_root_dir%/}/conf/Caddyfile" ]; then
-        _generate_config || return $?
-    fi
-    nohup ${_root_dir%/}/bin/caddy-`uname` -conf ${_root_dir%/}/conf/Caddyfile -root ${_root_dir%/}/web/public 1>${_root_dir%/}/log/caddy.out 2>${_root_dir%/}/log/caddy.err &
-    echo "$!" > ${_root_dir%/}/log/caddy.pid
-    _log "INFO" "Started web server (pid:`cat ${_root_dir%/}/log/caddy.pid`)"
-}
-
-function f_web_stop() {
-    local _root_dir="${1:-${_ROOT_DIR}}"
-    # TODO: should check if it's really caddy
-    kill `cat ${_root_dir%/}/log/caddy.pid`
-}
 
 
 ### (supposed to be) private functions #########################################
-function _generate_config() {
-    local _host="${1:-${_HOST}}"
-    local _port="${2:-${_PORT}}"
-    local _root_dir="${3:-${_ROOT_DIR}}"
-
-    if [ ! -d "${_root_dir%/}/conf" ]; then
-        if ! mkdir -p ${_root_dir%/}/conf; then
-            _log "ERROR" "Couldn't create ${_root_dir%/}/conf"
-            return 1
-        fi
-    fi
-
-    # At this moment, error is also stdout
-    echo ${_host}':'${_port}'
-gzip
-log stdout
-errors stdout
-fastcgi / 127.0.0.1:9000 php' > ${_root_dir%/}/conf/Caddyfile
-#forwardproxy { acl { allow all } }
-}
-
-function _is_in_path() {
-    local _cmd="$1"
-    local _err_msg="${2-"${_cmd} is not in PATH"}"
-    if ! which ${_cmd} &>/dev/null; then
-        [ -n "${_err_msg}" ] && _log "ERROR" "${_err_msg}"
-        return 1
-    fi
-}
-
-function _exist() {
-    local _path="$1"
-    local _err_msg="${2-"${_path} does not exist or empty."}"
-    if [ ! -s "${_path}" ]; then
-        [ -n "${_err_msg}" ] && _log "ERROR" "${_err_msg}"
-        return 1
-    fi
-}
-
-function _log() {
-    if [ -n "${_LOG_FILE_PATH}" ]; then
-        echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] $@" | tee -a ${_LOG_FILE_PATH} 1>&2
-    else
-        echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] $@" 1>&2
-    fi
-}
 
 
 ### main() ####################################################################
@@ -128,8 +50,12 @@ if [ "$0" = "$BASH_SOURCE" ]; then
         exit $?
     fi
 
-    # Step 1: Make sure all necessary commands are installed.
-    #         As how to install is diff by OS, not installing but just error.
-    f_validations || exit $?
-    f_web_start || exit $?
+    # Install necessary commands for Ubuntu as root (or sudo)
+    f_prepare
+
+    # Create non root user for the application
+    f_useradd "${_APP_USER}" || exit $?
+
+    # As this is python based application, setup python for the app user
+    sudo -u "${_APP_USER}" -i bash $BASH_SOURCE -f f_setup_python || exit $?
 fi
